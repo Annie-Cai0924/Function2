50c12391c346ac83bd3745d7b87ceea5305c90fb
Annie-Cai0924 <caiy248@newschool.edu>
2025-04-16 20:53:43 +0800

    Update moodData.js

--- moodData.js
+++ moodData.js
@@ -39,6 +39,8 @@ const emotions = [
 ];
 
 // Create a mood entry
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object
+//It is a "record generator". Each time the user clicks "Record mood," this function creates a "complete data object" with time, mood, and intensity. You can then display this data on a web page, save it to localStorage, or draw it on a star chart
 const createMoodEntry = (emotion, intensity) => {
     return {
         timestamp: Date.now(),
@@ -51,13 +53,20 @@ const createMoodEntry = (emotion, intensity) => {
 // Local storage key
 const STORAGE_KEY = "stellar_mood_tracker_data";
 
+//https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+//This is an arrow function called saveMoodEntries, which means "save emotional records." It accepts one parameter, entries, which is an array of all the sentiment data
 // Save mood entries to local storage
+//Convert entries (the array of mood records) to strings and store them in the browser with localStorage.setItem(), STORAGE_KEY. Since localStorage can only store strings, we use JSON.stringify() to turn the array into a string. The next time you want to read this data, just use JSON.parse() in loadMoodEntries() to get it back
 const saveMoodEntries = (entries) => {
     localStorage.setItem(STORAGE_KEY, JSON.stringify(entries));
 };
 
+//https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+//Let the page load as soon as you can recover previously recorded emotional data
 // Load mood entries from local storage
+//This is an arrow function called loadMoodEntries, which means load mood record.
 const loadMoodEntries = () => {
     const storedData = localStorage.getItem(STORAGE_KEY);
+    //If storedData has a value (indicating that it did have a record before), convert it to an array with JSON.parse() and return it.
     return storedData ? JSON.parse(storedData) : [];
 };
